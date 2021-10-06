cb3f558ed27bc37542642e4d054bebec8613647f
Tyler Thompson <GitKraken@dls0.com>
2021-10-06 16:41:26 -0700

    Captures console output

---
+++ Dojo.Tests/TestBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Dojo.Tests
+{
+    public class TestBase
+    {
+        protected readonly ITestOutputHelper output;
+
+        protected TestBase(ITestOutputHelper output)
+        {
+            this.output = output;
+            Console.SetOut(new Converter(output));
+        }
+
+        private class Converter : TextWriter
+        {
+            private readonly ITestOutputHelper _output;
+
+            public Converter(ITestOutputHelper output)
+            {
+                _output = output;
+            }
+
+            public override Encoding Encoding => Encoding.UTF8;
+
+            public override void WriteLine(string message)
+            {
+                _output.WriteLine(message);
+            }
+
+            public override void WriteLine(string format, params object[] args)
+            {
+                _output.WriteLine(format, args);
+            }
+
+            public override void Write(char value)
+            {
+                throw new NotSupportedException("This text writer only supports WriteLine(string) and WriteLine(string, params object[]).");
+            }
+        }
+    }
+}
--- Dojo/Rooms/Balanced.cs
+++ Dojo/Rooms/Balanced.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Dojo.Rooms
 {
@@ -6,7 +8,157 @@ namespace Dojo.Rooms
     {
         public bool IsBalanced(string expression)
         {
-            throw new NotImplementedException();
+            //return IsBalancedWithCounter(expression);
+            //return IsBalancedWithValueCounters(expression, counters);
+            return IsBalancedWithStack(expression, pairs);
         }
+
+        #region With Counter
+
+        private bool IsBalancedWithCounter(string expression)
+        {
+            var parenthesesCount = 0;
+            var squareBraceCount = 0;
+            var angleBracketCount = 0;
+            var curlyBraceCount = 0;
+
+            foreach (var character in expression)
+            {
+                UpdateCount(ref parenthesesCount, character, '(', ')');
+                UpdateCount(ref squareBraceCount, character, '[', ']');
+                UpdateCount(ref angleBracketCount, character, '<', '>');
+                UpdateCount(ref curlyBraceCount, character, '{', '}');
+
+                if (parenthesesCount < 0 ||
+                    squareBraceCount < 0 ||
+                    angleBracketCount < 0 ||
+                    curlyBraceCount < 0
+                )
+                {
+                    return false;
+                }
+            }
+
+            return parenthesesCount == 0 &&
+                   squareBraceCount == 0 &&
+                   angleBracketCount == 0 &&
+                   curlyBraceCount == 0;
+        }
+
+        private void UpdateCount(ref int counter, char character, char open, char close)
+        {
+            if (character == open)
+            {
+                counter++;
+            }
+
+            if (character == close)
+            {
+                counter--;
+            }
+        }
+
+        #endregion
+
+        #region With Value Counters
+
+        private readonly List<Counter> counters = new List<Counter>
+        {
+            new Counter('(', ')'),
+            new Counter('[', ']'),
+            new Counter('{', '}'),
+            new Counter('<', '>')
+        };
+
+        private class Counter
+        {
+            private readonly char open;
+            private readonly char close;
+            private int counter;
+
+            public bool IsValid => counter >= 0;
+            public bool IsInvalid => !IsValid;
+            public bool IsBalanced => counter == 0;
+
+            public Counter(char open, char close)
+            {
+                this.open = open;
+                this.close = close;
+            }
+
+            public void UpdateCounter(char character)
+            {
+                if (character == open)
+                {
+                    counter++;
+                }
+
+                if (character == close)
+                {
+                    counter--;
+                }
+            }
+        }
+
+        private bool IsBalancedWithValueCounters(string expression, List<Counter> counters)
+        {
+            foreach (var character in expression)
+            {
+                counters.ForEach(counter => counter.UpdateCounter(character));
+
+                if (counters.Any(counter => counter.IsInvalid))
+                {
+                    return false;
+                }
+            }
+
+            return counters.TrueForAll(counter => counter.IsBalanced);
+        }
+
+        #endregion
+
+        #region With Stack
+
+        private readonly Dictionary<char, char> pairs = new Dictionary<char, char>
+        {
+            { '(', ')' },
+            { '[', ']' },
+            { '{', '}' },
+            { '<', '>' }
+        };
+
+        private bool IsBalancedWithStack(string expression, Dictionary<char, char> pairs)
+        {
+            var stack = new Stack<char>(expression.Length);
+
+            foreach (var character in expression)
+            {
+                if (pairs.ContainsKey(character))
+                {
+                    stack.Push(character);
+                }
+
+                if (pairs.ContainsValue(character))
+                {
+                    if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    var key = stack.Pop();
+                    if (pairs[key] != character)
+                    {
+                        return false;
+                    }
+
+                }
+
+                Console.WriteLine(stack.ToString());
+            }
+
+            return stack.Count == 0;
+        }
+
+        #endregion
     }
 }
