27b95add063c29540f11463361b61949d566c49d
Tyler Thompson <GitKraken@dls0.com>
2021-10-19 13:39:53 -0700

    Adds Multi-Dimensional and Uber tests

---
+++ Dojo.Tests/MultiDimensionalArrayTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dojo.Rooms;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dojo.Tests
+{
+    public class MultiDimensionalArrayTests : TestBase
+    {
+        /*
+        Summary
+            You are given an array of numbers and a column count. Convert the array of numbers into a multi-dimensional array
+        Input
+            numbers = [1,2,3,4,5,6]
+            columnCount = 2
+        Output
+            [[1,2],[3,4],[5,6]]
+         */
+
+        private readonly MultiDimensionalArray sut = new();
+        
+        public MultiDimensionalArrayTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Theory]
+        [InlineData(new[] { 7, 3, 1, -10, 3, 2 }, 2)]
+        [InlineData(new[] { 7, 3, 1, -10, 3, 2 }, 3)]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 4)]
+        public void Process(int[] numbers, int columnCount)
+        {
+            var result = sut.Process(numbers, columnCount);
+            var rowCount = numbers.Length / columnCount;
+            var valueIndex = 0;
+
+            for (int rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+                {
+                    result[rowIndex, columnIndex].ShouldBe(numbers[valueIndex]);
+                    valueIndex++;
+                }
+            }
+        }
+    }
+}
---
+++ Dojo.Tests/UberPoolTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dojo.Rooms;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Dojo.Tests
+{
+    public class UberPoolTests : TestBase
+    {
+        /*
+        Summary
+            You are given a two-dimensional integer list requested_trips containing [start_x, end_x, num_passengers], 
+            and an integer capacity. Each requested trip asks to pick up num_passengers passenger at start_x and drop them off at end_x. 
+            You also have a car with the given capacity, and start at x = 0.
+            
+            Given that you'd like to pick up every passenger and can only move right, 
+            return whether you can pick up and drop off everyone.
+
+        Input
+            trips = [
+                [1, 30, 2],
+                [3, 5, 3],
+                [5, 9, 3]
+                    ]
+            capacity = 6
+        Output
+            True
+         */
+
+        private readonly UberPool sut = new();
+
+        public UberPoolTests(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, 30, 2, 3, 5, 3, 5, 9, 3 }, 6, true)]
+        public void Process(int[] trips, int capacity, bool expectedResult)
+        {
+            var multiDimensionalArray = trips.ToMultiDimensionalArray(3);
+
+            var result = sut.Process(trips: multiDimensionalArray, capacity: capacity);
+
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
